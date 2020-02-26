09e8313232ac31e964590d5e2ef131cbec4d073d
Luis Hernandez <eleazar.hernandez27@gmail.com>
2020-02-26 16:47:08 -0400

    Estructura visual
    Todas las partes de las vistas que se repiten en cada una de las pantallas

---
+++ estructura/footer.php
@@ -0,0 +1,51 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+ 
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2014-2019 <a href="https://Cardealersystem.com">Car Dealer System C.A</a>.</strong>
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="../sources/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../sources/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- Select2 -->
+<script src="../sources/plugins/select2/js/select2.full.min.js"></script>
+<!-- Bootstrap4 Duallistbox -->
+<script src="../sources/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
+<!-- InputMask -->
+<script src="../sources/plugins/moment/moment.min.js"></script>
+<script src="../sources/plugins/inputmask/min/jquery.inputmask.bundle.min.js"></script>
+<!-- date-range-picker -->
+<script src="../sources/plugins/daterangepicker/daterangepicker.js"></script>
+<!-- bootstrap color picker -->
+<script src="../sources/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="../sources/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Bootstrap Switch -->
+<script src="../sources/plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../sources/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../sources/dist/js/demo.js"></script>
+<!-- Page script -->
+<script src="../sources/funciones.js"></script>
+<script src="../sources/alerta.js"></script>
+<script>
+  $(function () {
+    //Initialize Select2 Elements
+    $('.select2').select2()
+
+    //Initialize Select2 Elements
+    $('.select2bs4').select2({
+      theme: 'bootstrap4'
+    })
+  })
+</script>
+</body>
+</html>
---
+++ estructura/navbar.php
@@ -0,0 +1,17 @@
+   
+<nav class="main-header navbar navbar-expand navbar-white ">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+      </li>
+    </ul>
+
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+       <li>
+          <button type="submit" title="Cerrar Sesión" onClick="logout();" class="btn btn-default "><i class="fas fa-power-off"></i></button>
+       </li>
+    </ul>
+</nav>
